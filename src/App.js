fa4579d743c0462737a03339bfeba4c562a7e355
Nithin-anim <nithin@animaker.com>
2022-01-12 14:57:50 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import video from './assets/video.mp4';
+import AnimatedText from './components/AnimatedText';
 
-function App() {
+const App = () => {
+  const [text, setText] = useState('Dummy Text');
+  const [startTime, setStartTime] = useState(0);
+  const [endTime, setEndTime] = useState(0);
+  const [showAnimation, setShowAnimation] = useState(false);
+
+  const onChange = (e, type) => {
+    switch (type) {
+      case 'text':
+        setText(e.target.value);
+        break;
+      case 'start':
+        setStartTime(e.target.value);
+        break;
+      case 'end':
+        setEndTime(e.target.value);
+        break;
+      default:
+    }
+  }
+
+  const onVideoPlayed = () => {
+    setTimeout(() => {
+      setShowAnimation(true);
+    }, startTime * 1000);
+  }
+
+  const onVideoEnded = () => {
+    setShowAnimation(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GSAP Demo</h1>
+      <AnimatedText text={text} toggleAnimation={showAnimation} duartion={endTime} />
+      <video className={'player'} src={video} autoPlay controls width={'700px'} height={'400px'} onPlay={onVideoPlayed} onEnded={onVideoEnded} />
+      <div className={'field-container'}>
+        <label htmlFor={'text-input'}>Text</label>
+        <input id={'text-input'} className={'text-input'} type={'text'} value={text} onChange={(e) => onChange(e, 'text')} />
+      </div>
+      <div className={'field-container'}>
+        <label htmlFor={'start-input'}>Start At</label>
+        <input id={'start-input'} className={'text-input'} type={'number'} value={startTime} onChange={(e) => onChange(e, 'start')} />
+      </div>
+      <div className={'field-container'}>
+        <label htmlFor={'end-input'}>End At</label>
+        <input id={'end-input'} className={'text-input'} type={'number'} value={endTime} onChange={(e) => onChange(e, 'end')} />
+      </div>
     </div>
   );
 }
